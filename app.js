fea30b3c93b25140f1d85c4787f11b0c39cc8b29
vivek7156 <142485319+vivek7156@users.noreply.github.com>
2024-08-30 22:19:12 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ let msg = document.querySelector("#msg");
 
 let turnO = true;
 let count = 0;
+
+// Initialize scores from localStorage or default to 0
+let xWins = localStorage.getItem('xWins') ? parseInt(localStorage.getItem('xWins')) : 0;
+let oWins = localStorage.getItem('oWins') ? parseInt(localStorage.getItem('oWins')) : 0;
+
+document.getElementById("xWins").innerText = xWins;
+document.getElementById("oWins").innerText = oWins;
+
 const winPatterns = [
     [0, 1, 2],
     [0, 3, 6],
@@ -23,17 +31,27 @@ const resetGame = () => {
     enableBoxes();
     alertBox.classList.add("hide");
     msg.innerText = "";
-    boxes.forEach((box) => (box.innerText = ""));
-    newGameBtn.disabled = false; // Enable the "New Game" button
+    boxes.forEach((box) => {
+        box.innerText = "";
+        box.disabled = false;
+        box.classList.remove("winning-box");
+        box.classList.remove("x", "o");
+    });
+    newGameBtn.disabled = false;
 };
-
 
 const boxesArray = Array.from(boxes);
 
 boxesArray.forEach((box, index) => {
     box.addEventListener("click", () => {
         if (!box.innerText) {
-            box.innerText = turnO ? "O" : "X";
+            if (turnO) {
+                box.innerText = "O";
+                box.classList.add("o");
+            } else {
+                box.innerText = "X";
+                box.classList.add("x");
+            }
             turnO = !turnO;
             box.disabled = true;
             count++;
@@ -42,7 +60,7 @@ boxesArray.forEach((box, index) => {
             if (count === 9 && !isWinner) {
                 gameDraw();
             } else if (isWinner) {
-                newGameBtn.disabled = false; // Enable the "New Game" button when there's a winner
+                newGameBtn.disabled = false;
             }
         }
     });
@@ -62,24 +80,40 @@ const disableBoxes = () => {
 
 const enableBoxes = () => {
     boxesArray.forEach((box) => {
+        box.disabled = false;
     });
 };
 
-const showWinner = (winner) => {
+const showWinner = (winner, pattern) => {
+    if (winner === "X") {
+        xWins++;
+        localStorage.setItem('xWins', xWins);  // Store X wins in localStorage
+        document.getElementById("xWins").innerText = xWins;
+    } else if (winner === "O") {
+        oWins++;
+        localStorage.setItem('oWins', oWins);  // Store O wins in localStorage
+        document.getElementById("oWins").innerText = oWins;
+    }
+    
     msg.innerText = `Congratulations, Winner is ${winner}`;
     alertBox.classList.remove("hide");
+
+    pattern.forEach(index => {
+        boxesArray[index].classList.add("winning-box");
+    });
+
     disableBoxes();
 };
 
 const checkWinner = () => {
-    for (pattern of winPatterns) {
+    for (let pattern of winPatterns) {
         let pos1Val = boxesArray[pattern[0]].innerText;
         let pos2Val = boxesArray[pattern[1]].innerText;
         let pos3Val = boxesArray[pattern[2]].innerText;
 
-        if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
-            if (pos1Val == pos2Val && pos2Val == pos3Val) {
-                showWinner(pos1Val);
+        if (pos1Val !== "" && pos2Val !== "" && pos3Val !== "") {
+            if (pos1Val === pos2Val && pos2Val === pos3Val) {
+                showWinner(pos1Val, pattern);
                 return true;
             }
         }
